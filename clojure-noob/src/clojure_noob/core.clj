4b2c89ac3b91e4efb446b02589a391a4aedeec5e
Łukasz Lalik <lukasz.lalik@gmail.com>
2016-02-01 20:53:06 +0100

    Functions examples

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -148,9 +148,58 @@
         (hello-lists)
         (hello-sets)))
 
+
+;Functions
+(defn map-example
+    "Prints example result of map function"
+    []
+    (println "map result:" (map inc [1 2 3 4])))
+
+(defn hello-name
+    "Says hello to given recipient"
+    [recipient]
+    (println (str "Hello " recipient "!")))
+
+(defn multi-arity
+  "2 arguments"
+  ([a b]
+    (println "I got 2 arguments" a b))
+  ([a b c]
+    (println "I got 3 arugments" a b c)))
+
+(defn hello-default
+  "Greet given name"
+  ([name]
+    (println (str "Hello " name "!")))
+  ([]
+    (hello-default "World")))
+
+(defn greet-all
+  [& people]
+  (map hello-default people))
+
+(defn equipment
+  [name & items]
+  (println name "has in backpack:"
+    (clojure.string/join ", " items)))
+
+(defn hello-functions
+    "Varioud functions examples"
+    []
+    (do
+        (map-example)
+        (hello-name "K3nn7")
+        (multi-arity 1 2)
+        (multi-arity 1 2 3)
+        (hello-default)
+        (hello-default "K3nn7")
+        (greet-all "K3nn7" "Foo" "Bar")
+        (equipment "K3nn7" "axe" "shield" "armor")))
+
 (defn -main
   "Executes set of hello-world functions"
   [& args]
   (do
       (hello-forms)
-      (hello-data-structures)))
+      (hello-data-structures)
+      (hello-functions)))
