66ff968659ea6cac16c106dd1f46570205e7ed69
Łukasz Lalik <lukasz.lalik@gmail.com>
2016-02-03 22:31:42 +0100

    Looping and let

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -229,6 +229,50 @@
   (hello-user {:name "k3nn7" :email "k3nn7@example.com"})
   (hello-user-2 {:name "foo" :email "foo@example.com"}))
 
+;Hobbit example
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
+
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+          (into final-body-parts (set [part (matching-part part)])))))))
+
+;Looping
+(defn loop-example
+  []
+  (loop [iteration 0]
+    (println "Iteration" iteration)
+    (if (> iteration 3)
+      (println "Goodbye!")
+      (recur (inc iteration)))))
+
 (defn -main
   "Executes set of hello-world functions"
   [& args]
@@ -236,4 +280,6 @@
       (hello-forms)
       (hello-data-structures)
       (hello-functions)
-      (destructuring)))
+      (destructuring)
+      (symmetrize-body-parts asym-hobbit-body-parts)
+      (loop-example)))
